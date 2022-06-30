06d026ea5337adc3bdab249a0c9a57cd8a1fd824
Roberto Racz <robertorac94@hotmail.com>
2022-06-30 11:31:24 +0200

    combatService pasted

--- src/main/java/com/codecool/dungeoncrawl/logic/services/CombatService.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/services/CombatService.java
@@ -4,6 +4,11 @@ import com.codecool.dungeoncrawl.data.actors.Actor;
 
 public class CombatService {
     public void exchangeBlows(Actor fighter1, Actor fighter2) {
-
+        int fighter1Power = fighter1.getStrength();
+        int fighter2Power = fighter2.getStrength();
+        int player1Health = fighter1.getHealth();
+        int fighter2Health = fighter2.getHealth();
+        fighter1.setHealth(player1Health - fighter2Power);
+        fighter2.setHealth(fighter2Health - fighter1Power);
     }
 }
