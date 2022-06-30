31bf1e2c9e830ebadfa2fd32c8927475f67a68bf
elStriker <briankouophe38@gmail.com>
2022-06-30 12:05:05 +0200

    working on refactoring RestrictMovement

--- src/main/java/com/codecool/dungeoncrawl/logic/RestrictMovement.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/RestrictMovement.java
@@ -1,13 +1,25 @@
 package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.actors.Actor;
 import com.codecool.dungeoncrawl.data.actors.Skeleton;
 
 //refactor the class
 public class RestrictMovement {
     public boolean isValidMove(Cell nextCell) {
-        return !((nextCell.getTileName().equals("wall")) ||
-                (nextCell.getActor() instanceof Skeleton) ||
-                (nextCell.getTileName().equals("closeddoor")));
+//        return !((nextCell.getTileName().equals("wall")) ||
+//                (nextCell.getActor() instanceof Skeleton) ||
+//                (nextCell.getTileName().equals("closeddoor")));
+        boolean value = false;
+
+        if (nextCell.getTileName().equals("wall"))
+        {
+            return value;
+        } else if (nextCell.getTileName().equals("closeddoor")) {
+
+
+        } else if (nextCell.getActor() instanceof Actor){
+
+        }
     }
 }
