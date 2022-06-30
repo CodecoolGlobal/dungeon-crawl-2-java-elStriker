95e15f24da8bdc34b9b511a3d533bbfe25e319dc
elStriker <briankouophe38@gmail.com>
2022-06-30 13:46:21 +0200

    Restrictmovement is impl

--- src/main/java/com/codecool/dungeoncrawl/logic/RestrictMovement.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/RestrictMovement.java
@@ -1,25 +1,31 @@
 package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
+import com.codecool.dungeoncrawl.data.PlayerInventory;
 import com.codecool.dungeoncrawl.data.actors.Actor;
-import com.codecool.dungeoncrawl.data.actors.Skeleton;
 
 //refactor the class
 public class RestrictMovement {
-    public boolean isValidMove(Cell nextCell) {
-        return !((nextCell.getTileName().equals("wall")) ||
-                (nextCell.getActor() instanceof Skeleton) ||
-                (nextCell.getTileName().equals("closeddoor")));
-//        boolean value = false;
-//
-//        if (nextCell.getTileName().equals("wall"))
-//        {
-//            return value;
-//        } else if (nextCell.getTileName().equals("closeddoor")) {
-//
-//
-//        } else if (nextCell.getActor() instanceof Actor){
-//
-//        }
+    public boolean isValidMove(Cell nextCell, GameMap map) {
+        PlayerInventory inventory = new PlayerInventory();
+        InventoryService inventoryService = new InventoryService(inventory);
+        CombatService combatService = new CombatService();
+//        return !((nextCell.getTileName().equals("wall")) ||
+//                (nextCell.getActor() instanceof Skeleton) ||
+//                (nextCell.getTileName().equals("closeddoor")));
+        boolean value = false;
+
+        if (nextCell.getTileName().equals("wall")) {}
+
+        else if (nextCell.getTileName().equals("closeddoor")) {
+            if (inventoryService.hasKey()) {
+                map.getCell(nextCell.getX(), nextCell.getY()).setType(CellType.OPENDOOR);
+                value = true;
+            }
+        }else if (nextCell.getActor() instanceof Actor){
+            combatService.exchangeBlows(nextCell.getActor(), map.getPlayer());
+        }
+        return value;
     }
 }
